18213becfd8ad6498265942336210f171cc84189
Michał Niedziółka <mn406264@students.mimuw.edu.pl>
2018-10-15 14:34:43 +0200

    Cos sie zle dodalo

--- lista.ml
+++ lista.ml
@@ -3,4 +3,10 @@ let rec buduj n =
         else n :: buduj (n - 1)
 ;;
 
+let rec budujogonowo n =
+        let rec pom n l =
+                if n = 0 then l
+                else pom (n - 1) (n :: l)
+        in pom n []
+;;
 
