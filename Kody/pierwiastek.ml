a71d84847f51d462a4d17f81c9408bfbfa37f38f
Michał Niedziółka <mn406264@green06.mimuw.edu.pl>
2018-10-17 14:57:49 +0200

    Jeszcze jeden commit bo sie pomylilem

--- Kody/pierwiastek.ml
+++ Kody/pierwiastek.ml
@@ -1,6 +1,5 @@
 let sqrt n = 
         let rec temp k i =
                 if i > n then k - 1 else temp (k + 1) (i + 2 * k + 1)
-        in temp 0 0;;
-
-print_int (sqrt 8);;
+        in temp 0 0
+;;
