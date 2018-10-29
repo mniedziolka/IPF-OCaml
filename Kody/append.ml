3e89e9d26e21e57089122146e30cacbe1a012d4b
Michał Niedziółka <michalekniedziolka@gmail.com>
2018-10-29 15:46:38 +0100

    Następny append

--- Kody/append.ml
+++ Kody/append.ml
@@ -1,2 +1,6 @@
-let append r li1 li2 =
+let appendr r li1 li2 =
     fold_right (fun h a -> h::a) li1 li2;;
+
+let appendl l1 l2 = 
+    let reverse xs = List.fold_left (fun y ys -> y::ys) [] xs
+    in reverse (List.fold_left(fun y ys -> y::ys) (reverse li1) l2);;
