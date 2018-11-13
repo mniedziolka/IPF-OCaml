b812b3082767a80b3f20a3c621507db37dcb86ad
Michał Niedziółka <michalekniedziolka@gmail.com>
2018-11-13 20:13:22 +0100

    ostatni?

--- Drzewa lewicowe/leftist.ml
+++ Drzewa lewicowe/leftist.ml
@@ -1,216 +1,38 @@
-(* Drzewa lewicowe                *)
-(* Autor: Michał Niedziółka       *)
-(* Code review: Marcin Abramowicz *)
+type 'a queue = 
+    | Null
+    | Node of 'a queue * 'a * 'a queue * int;;
 
-
-(* node * wysokość * lewy syn * prawy syn *)
-type 'a queue =
-  | Node of 'a * int * 'a queue * 'a queue
-  | Null
-
-exception Empty
-
-(* wyłuskiwanie poszczególnych wartości *)
-let value n = 
-    match n with 
-        | Null -> raise Empty
-        | Node(v,_,_,_) -> v
-let height n =
-    match n with 
-        | Null -> 0
-        | Node(_,h,_,_) -> h
-let left n =
-    match n with 
-        | Null -> raise Empty
-        | Node(_,_,l,_) -> l
-let right n =
-    match n with 
-        | Null -> raise Empty
-        | Node(_,_,_,r) -> r
-
-
-(* puste drzewo *)
 let empty = Null
-(* liść czyli node bez synów *)
-let leaf n = Node(n, 1, Null, Null)
 
+exception Empty;;
 
-let is_empty n = 
-    match n with
-        | Null -> true
-        | Node(_,_,_,_) -> false
+let is_empty a = 
+    a = Null;;
 
-let rec join a b =
-    if is_empty a then b
-    else if is_empty b then a
-    else if value a > value b then join b a
-    else let c = join (right a) b 
-    in if height (left a) < height c
-    then Node (value a, (height a) + 1, c, left a)
-    else Node (value a, (height c) + 1, left a, c)
+let getrdepth a = 
+    match a with
+    | Null -> -1
+    | Node (, , , d) -> d;;
 
-let add e q = join (leaf e) q
+let rec join a b = 
+    match a, b with
+    | Null,  -> b
+    | , Null -> a
+    | Node (la, va, ra, ha), Node (lb, vb, rb, hb) ->
+        if va > vb then join b a else
+        let new_subtree = join ra b in
+        let ldepth = get_rdepth la and newdepth = get_rdepth new_subtree in
+        if ldepth < newdepth then
+            Node (la, va, new_subtree, newdepth + 1)
+        else
+            Node (new_subtree, va, la, ldepth + 1);;
 
-let delete_min q = 
-    if is_empty q then raise Empty
-    else (value q, join (left q) (right q)) 
+let add e q = 
+    let aux_queue = Node (Null, e, Null, 1) in
+    join aux_queue q;;
 
-
-let a = empty;;
-let b = add 1 empty;;
-
-assert (is_empty a = true);;
-assert (try let _=delete_min a in false with Empty -> true);;
-assert (is_empty b <> true);;
-
-let b = join a b ;;
-assert (is_empty b <> true);;
-
-let (x,y) = delete_min b;;
-
-assert (x = 1);;
-assert (is_empty y = true);;
-assert (try let _=delete_min y in false with Empty -> true);;
-
-(* delete_min integer tests *)
-let b = add 1 empty;;
-let b = add 3 b;;
-let b = add (-1) b;;
-let b = add 2 b;;
-let b = add 1 b;;
-
-let (a,b) = delete_min b;;
-assert (a = -1);;
-
-let (a,b) = delete_min b;;
-assert (a = 1);;
-
-let (a,b) = delete_min b;;
-assert (a = 1);;
-
-let (a,b) = delete_min b;;
-assert (a = 2);;
-
-let (a,b) = delete_min b;;
-assert (a = 3);;
-
-assert(is_empty b = true);;
-
-(* delete_min string tests *)
-let b = add "a" empty;;
-let b = add "aca" b;;
-let b = add "nzbzad" b;;
-let b = add "nzbza" b;;
-let b = add "bxbxc" b;;
-
-let (a,b) = delete_min b;;
-assert (a = "a");;
-
-let (a,b) = delete_min b;;
-assert (a = "aca");;
-
-let (a,b) = delete_min b;;
-assert (a = "bxbxc");;
-
-let (a,b) = delete_min b;;
-assert (a = "nzbza");;
-
-let (a,b) = delete_min b;;
-assert (a = "nzbzad");;
-
-assert(is_empty b = true);;
-assert (try let _=delete_min b in false with Empty -> true);;
-
-(* join tests *)
-
-let b = add 1 empty;;
-let b = add 3 b;;
-let b = add (-1) b;;
-let b = add 2 b;;
-let b = add 1 b;;
-
-let c = add 10 empty;;
-let c = add (-5) c;;
-let c = add 1 c;;
-let c = add 4 c;;
-let c = add 0 c;;
-
-let b = join b c;;
-
-let (a,b) = delete_min b;;
-assert (a = (-5));;
-
-let (a,b) = delete_min b;;
-assert (a = (-1));;
-
-let (a,b) = delete_min b;;
-assert (a = 0);;
-
-let (a,b) = delete_min b;;
-assert (a = 1);;
-
-let (a,b) = delete_min b;;
-assert (a = 1);;
-
-let (a,b) = delete_min b;;
-assert (a = 1);;
-
-let (a,b) = delete_min b;;
-assert (a = 2);;
-
-let (a,b) = delete_min b;;
-assert (a = 3);;
-
-let (a,b) = delete_min b;;
-assert (a = 4);;
-
-let (a,b) = delete_min b;;
-assert (a = 10);;
-
-assert (try let _=delete_min b in false with Empty -> true);;
-
-let b = add 1 empty;;
-let b = add 3 b;;
-let b = add (-1) b;;
-let b = add 2 b;;
-let b = add 1 b;;
-
-let c = add 10 empty;;
-let c = add (-5) c;;
-let c = add 1 c;;
-let c = add 4 c;;
-let c = add 0 c;;
-
-let b = join c b;;
-
-let (a,b) = delete_min b;;
-assert (a = (-5));;
-
-let (a,b) = delete_min b;;
-assert (a = (-1));;
-
-let (a,b) = delete_min b;;
-assert (a = 0);;
-
-let (a,b) = delete_min b;;
-assert (a = 1);;
-
-let (a,b) = delete_min b;;
-assert (a = 1);;
-
-let (a,b) = delete_min b;;
-assert (a = 1);;
-
-let (a,b) = delete_min b;;
-assert (a = 2);;
-
-let (a,b) = delete_min b;;
-assert (a = 3);;
-
-let (a,b) = delete_min b;;
-assert (a = 4);;
-
-let (a,b) = delete_min b;;
-assert (a = 10);;
-
-assert (try let _=delete_min b in false with Empty -> true);;
+let deletemin q = 
+    match q with
+    | Null -> raise Empty
+    | Node (l, v, r, ) ->
+        (v, (join l r));;
