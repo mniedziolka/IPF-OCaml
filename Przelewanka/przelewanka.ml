0e8904c1da90b9e58192a4ade00b903c8f9b9214
Michał Niedziółka <michalekniedziolka@gmail.com>
2019-01-17 00:03:49 +0100

    Dziala, razem z optymalizacjami

--- Przelewanka/przelewanka.ml
+++ Przelewanka/przelewanka.ml
@@ -3,22 +3,32 @@
 (* Code review: Kajetan Husiatyński *)
 
 let przelewanka tab = 
+	let rec gcd a b =
+        if a = 0 then b
+        else gcd (b mod a) a
+    in let gcd_all = Array.fold_left (fun a (x, _) -> gcd (min x a) (max x a)) 0 tab in
+
+    (* Rozwiązanie nie istnieje jeśli nie ma ani jednej pustej/pełnej szklanki *)
+    (* Rozwiązanie nie istnieje jeśli istnieje szklanka w której końcowa ilość wody nie jest podzielna przez NWD pojemności *)
 	let exists tab = 
 		let check = ref 0 in
 		begin
 		for i = 0 to ((Array.length tab) - 1) do
-			if fst tab.(i) == 0 || fst tab.(i) == snd tab.(i) then check := 1;
+			if snd tab.(i) == 0 || fst tab.(i) == snd tab.(i) then check := 1;
 		done;
+		
+		for i = 0 to ((Array.length tab) - 1) do
+			if snd tab.(i) mod gcd_all <> 0 then check := 0;
+		done;
+
 		!check
 		end
 
 	and solve tab = 
-		let refill k state = begin state.(k) <- fst tab.(k); end; state
-		and spill k state = begin state.(k) <- 0; end; state
-		and pour k state = 
+		let pour k state = 
+		let listates = ref [] in
 		begin
-			let listates = ref []
-			and curr = Array.copy state in
+			let curr = Array.copy state in
 			for i = 0 to ((Array.length state) - 1)do
 				let state = Array.copy curr in
 				let size = (fst tab.(i)) - curr.(i) in
@@ -37,6 +47,8 @@ let przelewanka tab =
 			done;	
 		end;	!listates
 
+		(* Trzymam hashe wszystkich stanów które kiedykolwiek odwiedziłem żeby zapobiec niepotrzebnym odwiedzeniom *)
+		(* Zaczynam od stanu zero czyli wszystkie szklanki puste *)
 		in let hash_table = Hashtbl.create 1488997 
 		and queue = Queue.create () 
 		and state = Array.make (Array.length tab) 0 
@@ -47,32 +59,31 @@ let przelewanka tab =
 			for i = 0 to ((Array.length tab) - 1) do
 				hash.(i) <- snd tab.(i);
 			done;
+			(* Chodzę BFSem dopóki nie przejdę wszystkich możliwych stanów lub nie znajdę rozwiązania *)
 			Hashtbl.add hash_table hash (-1);
-			Hashtbl.add hash_table state 0;
+			if Hashtbl.mem hash_table state then begin flag := 1; end
+			else Hashtbl.add hash_table state 0;
 			Queue.add state queue;
 			while (not (Queue.is_empty queue)) && !flag = 0 do
 				let curr = Queue.take queue in
 				let dist = Hashtbl.find hash_table curr in
 				begin
-					print_int 1;
 					for i = 0 to ((Array.length curr) - 1) do
-						let re = refill i curr and sp = spill i curr and po = pour i curr in
+						let re = Array.copy curr and sp = Array.copy curr and po = pour i curr in
 						begin
-							for j = 0 to ((Array.length re) - 1) do
-								print_int re.(j)
-							done;
+							re.(i) <- fst tab.(i);
+							sp.(i) <- 0;
+
 							List.iter (fun el -> if (not (Hashtbl.mem hash_table el)) then begin Hashtbl.add hash_table el (dist+1); Queue.add el queue; end
 																	   else if Hashtbl.find hash_table el = (-1) then 
 																	   begin flag := 1; result := (dist + 1); end 
-																	   else (for j = 0 to ((Array.length el) - 1) do print_int el.(j) done;)) (re::(sp::po));
+																	   else ()) (re::(sp::po));
 						end
 					done 
 				end
 			done;
-			!result
+			if !flag = 0 then (-1) else !result
 		end
 
 
-	in if exists tab = 1 then solve tab else (-1)
-
-(* przelewanka [|(3,2);(3,3);(1,0);(12,1)|];; *)
+	in if gcd_all = 0 then 0 else if exists tab = 1 then solve tab else if (Array.length tab) = 0 then 0 else (-1)
